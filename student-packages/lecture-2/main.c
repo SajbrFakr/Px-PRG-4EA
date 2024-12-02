d921e3c509573bbb323e870cfa950d9863eddbd9
SajbrFakr <janjakub.dvorak2005@gmail.com>
2024-12-02 12:05:46 +0100

    debounce

--- student-packages/lecture-2/main.c
+++ student-packages/lecture-2/main.c
@@ -9,32 +9,39 @@
 #define BUTTONe 21
 
 void handler(uint gpio, uint32_t events);
+int counter = 0;
+int lastCounter = 0;
+int delayTime = 20;
 
 int main()
 {
     // Initialize all standard I/O
     stdio_init_all();
-    sleep_ms(1000);
+    sleep_ms(5000);
     printf("Starting...\n");
 
     // Initialize GPIO pins
     gpio_init(BUTTONd);
-    gpio_set_dir(BUTTONd, GPIO_OUT);
+    gpio_set_dir(BUTTONd, GPIO_IN);
+    gpio_pull_up(BUTTONd);
 
     gpio_init(BUTTONe);
     gpio_set_dir(BUTTONe, GPIO_IN);
+    gpio_pull_up(BUTTONe);
 
     // Enable interrupts for button pin
     gpio_set_irq_enabled_with_callback(BUTTONd, GPIO_IRQ_EDGE_RISE, true, &handler);
     gpio_set_irq_enabled_with_callback(BUTTONe, GPIO_IRQ_EDGE_RISE, true, &handler);
 
-    // Initialize variables
-    int counter = 0;
-    int lastCounter = counter;
-
-    // Printing counter state if there is a change
-    if(lastCounter != counter){
-        printf(\ncounter);
+    while (1)
+    {
+        sleep_ms(100);
+        if(lastCounter != counter) {
+            printf("\r%i", counter);
+            lastCounter = counter;
+        }
+        
+        unsigned long time = to_ms_since_boot(get_absolute_time());
     }
 
     return 0;
@@ -42,7 +49,10 @@ int main()
 
 void handler(uint gpio, uint32_t events)
 {
-    if(gpio == BUTTONd) counter--;
-    if(gpio == BUTTONe) counter++;
-    lastCounter = counter;
+    if(gpio == BUTTONd && time - to_ms_since_boot(get_absolute_time()) >= delayTime) {
+        counter--;
+    }
+    else if(gpio == BUTTONe && time - to_ms_since_boot(get_absolute_time()) >= delayTime) {
+        counter++;
+    }
 }
